b045f126e181953624316f03346ec33332b2c23f
Andrey Cortez <andrey.cortez12@gmail.com>
2025-10-16 08:53:53 -0300

    montecarlo sequencial

--- src/pi_montecarlo.c
+++ src/pi_montecarlo.c
@@ -1,7 +1,23 @@
 #include "stdio.h"
+#include "random.h"
+
+
+#define M 10000
 
 int main()
 {
-    printf("hello world!!\n");
+    srand(49);
+
+    int dentro = 0;
+
+    for (int i = 0; i < M; i++)
+    {
+        x = (float)rand()/RAND_MAX;
+        y = (float)rand()/RAND_MAX;
+
+        if (x*x + y*y <= 1) dentro += 1;
+    }
+
+    printf("%f\n", float(dentro)/M);
     return 0;
 }
